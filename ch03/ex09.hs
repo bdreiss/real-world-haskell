b714d8063d052c636a0878b81894c86f7262486e
bdreiss <63782097+bdreiss@users.noreply.github.com>
2022-02-03 12:44:47 +0100

    Update ex09.hs
    Including guard when list is only one item long, so that there is no seperator added after last item: "The seperator should appear between elements of the list, but it should not follow the last element."

--- ch03/ex09.hs
+++ ch03/ex09.hs
@@ -3,4 +3,5 @@
 intersperse :: a -> [[a]] -> [a]
 intersperse s xs = intersperseAcc [] xs
   where intersperseAcc acc [] = acc
+        intersperseAcc acc xs | length xs == 1 = acc ++ (head xs)
         intersperseAcc acc (h:t) = intersperseAcc (acc ++ h ++ [s]) t
